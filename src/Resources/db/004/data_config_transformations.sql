945f54c4c851a55f10b7f25f8200ad98686b1f88
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2019-01-23 14:07:07 +0100

    Added sql files for default data.

---
+++ src/Resources/db/004/data_config_transformations.sql
@@ -0,0 +1,3 @@
+INSERT INTO config_transformations (id, value, unit, inverse, fromConfig_id, toConfig_id) VALUES
+  (NULL, 10, '%', 0, 42, 43)
+;
---
+++ src/Resources/db/005/data_config_subsidies.sql
@@ -0,0 +1,9 @@
+INSERT INTO config_subsidies (subsidy_id, config_id) VALUES
+
+(91, 43),
+(92, 43),
+(93, 43),
+(94, 43),
+(95, 43)
+
+;
